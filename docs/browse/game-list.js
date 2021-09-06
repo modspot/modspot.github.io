2246e525844a971d852aa84155b6fae17cc549ff
Aelto <Aelto@users.noreply.github.com>
2021-09-06 20:30:54 +0200

    fix the dark souls link in the game list

--- docs/browse/game-list.js
+++ docs/browse/game-list.js
@@ -5,7 +5,7 @@ export default function GameList() {
     { route: 'witcher-3', name: 'Witcher 3' },
     { route: 'cyberpunk2077', name: 'Cyberpunk 2077' },
     { route: 'valheim', name: 'Valheim' },
-    { route: 'dark', name: 'Dark Souls' },
+    { route: 'dark-souls', name: 'Dark Souls' },
     { route: 'minecraft', name: 'Minecraft' },
     { route: 'skyrim', name: 'Skyrim' },
     { route: 'fallout', name: 'Fallout' },
